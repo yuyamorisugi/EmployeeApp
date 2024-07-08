6e7e96089beedc6161099c8ea2996c1c8a8b247f
Yuya_Morisugi <yuya.morisugi.yaz@gmail.com>
2024-07-08 13:16:30 +0900

    ユーザーインタフェースの作成。

---
+++ app/src/main/java/com/example/employeeapp/EmployeeRegisterFragment.kt
@@ -0,0 +1,32 @@
+package com.example.employeeapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+class EmployeeRegisterFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // フラグメントのレイアウトをインフレート
+        return inflater.inflate(R.layout.fragment_employee_register, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 戻るボタンの処理を追加する
+        val backButton = view.findViewById<Button>(R.id.button)
+        backButton.setOnClickListener {
+            // EmployeeListFragmentに遷移する
+            val fragment = EmployeeListFragment()
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit()
+        }
+    }
+}
